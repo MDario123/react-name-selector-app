32f2b2bd4f85a6d8a518521896c41bd4ee9ddb5e
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-04 19:34:23 +0100

    dev: add the basic structure for the data

---
+++ src/assets/data.ts
@@ -0,0 +1,15 @@
+export type BabyName = {
+  name: string;
+  etimology: string;
+};
+
+export const babyNames: BabyName[] = [
+  {
+    name: "Sophia",
+    etimology: `Means "wisdom" in Greek. This was the name of an early, probably mythical, saint who died of grief after her three daughters were martyred during the reign of the emperor Hadrian. Legends about her probably arose as a result of a medieval misunderstanding of the phrase Hagia Sophia "Holy Wisdom", which is the name of a large basilica in Constantinople.`,
+  },
+  {
+    name: "Jackson",
+    etimology: `From an English surname meaning "son of Jack". A famous bearer of the surname was American president Andrew Jackson (1767-1845).`,
+  },
+];
