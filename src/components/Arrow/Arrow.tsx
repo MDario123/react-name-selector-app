69c95f397347b4c4e746dda12ba84a83ddb7e146
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-08 23:56:43 +0100

    progress

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,14 +1,22 @@
+import { motion, useScroll } from "motion/react";
 import styles from "./Arrow.module.css";
 
+const variants = {
+  start: {},
+  other: {},
+};
+
 export function Arrow() {
+  const { scrollYProgress } = useScroll();
   return (
-    <svg
+    <motion.svg
       version="1.1"
       width="120"
       height="80"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 120 80"
       className={styles.arrow}
+      variants={variants}
     >
       <polyline
         points="20 20 60 60 100 20"
@@ -18,6 +26,6 @@ export function Arrow() {
         fill="none"
         strokeLinejoin="round"
       />
-    </svg>
+    </motion.svg>
   );
 }
