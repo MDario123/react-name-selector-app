22a15ba6c246103f9f60d3179b9323c249ded43d
MDario123 <manuel.dario.oliver@gmail.com>
2024-12-08 23:57:07 +0100

    chore: remove unused code

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -1,22 +1,14 @@
-import { motion, useScroll } from "motion/react";
 import styles from "./Arrow.module.css";
 
-const variants = {
-  start: {},
-  other: {},
-};
-
 export function Arrow() {
-  const { scrollYProgress } = useScroll();
   return (
-    <motion.svg
+    <svg
       version="1.1"
       width="120"
       height="80"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 120 80"
       className={styles.arrow}
-      variants={variants}
     >
       <polyline
         points="20 20 60 60 100 20"
@@ -26,6 +18,6 @@ export function Arrow() {
         fill="none"
         strokeLinejoin="round"
       />
-    </motion.svg>
+    </svg>
   );
 }
